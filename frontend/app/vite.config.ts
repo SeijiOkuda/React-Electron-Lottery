2dde3e22891528908529fa8ccbb10c30d1ccb3eb
Seiji Okuda <seijiokuda507@gmail.com>
2024-12-24 21:47:49 +0900

    mod:elecrtonでの構築

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -4,16 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //ホスト側がdockerコンテナのポート5173にアクセスできるようにする
   server: {
-    watch: {
-      usePolling: true, // Docker環境でのファイル変更検出を改善
-    },
-    host: true,          // これでViteサーバーがホストされるIPアドレスにバインドされます
-    strictPort: true,    // ポートの競合がある場合でも、指定したポートを使う
-    port: 5173,          // 使用するポート（例：5173）
-    hmr: {
-      host: 'localhost', // ホットリロードのホスト
-    },
+    open: true, // 開発サーバーが自動でブラウザを開くようにする
+  },
+  build: {
+    outDir: 'dist',
   },
 })
